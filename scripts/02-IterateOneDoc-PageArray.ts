830fbbec47eaa52378202d6abd8275e63c8ed62c
Zac Saber <zazer.unsw@gmail.com>
2024-08-18 09:59:17 +1000

    initial commit

--- scripts/02-IterateOneDoc-PageArray.ts
+++ scripts/02-IterateOneDoc-PageArray.ts
@@ -1,22 +1,10 @@
-// there are multiple modes to add as header: //nobundling //native //npm //nodejs
-// https://www.windmill.dev/docs/getting_started/scripts_quickstart/typescript#modes
-
-// import { toWords } from "number-to-words@1"
 import * as wmill from "windmill-client"
 import { S3Object } from 'windmill-client';
-
-
-// fill the type, or use the +Resource type to get a type-safe reference to a resource
-// type Postgresql = object
 
 type Page = {
   content: string;
   confidence: number;
 }
-
-// type DocJSON = {
-//   content: Page[]
-// }
 
 export async function main(
   s3JsonPath: string,
@@ -29,21 +17,10 @@ export async function main(
   const decoder = new TextDecoder();
   const fileJson = JSON.parse(decoder.decode(file_content))
 
-  // // Or load the file lazily as a Blob
-  // let fileContentBlob = await wmill.loadS3FileStream(input_file);
-  // console.log(await fileContentBlob.text());
-
-
   const docPages = fileJson // fmt: [ {text: XXX }, {text: XXX }, ... ]
-  console.log(docPages)
 
   const pageStrings = docPages.map((page) => page.text);
-
-  // const pageConfidenceTotals = docPages.reduce((total, page) => total + page.confidence, 0);
-
   return {
     pageStrings,
   }
-
-  // return { foo: firstPage };
 }
